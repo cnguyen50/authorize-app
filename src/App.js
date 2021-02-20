06242901dc3fc8a50cb746bafebce1f0d6fd11c8
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-02-19 19:46:46 -0800

    config firebase

--- src/App.js
+++ src/App.js
@@ -4,6 +4,20 @@ import { Header } from './components/common'
 import firebase from 'firebase'
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCZuaNczqdp-jrwmEczY_NiZiQkQHtG6cY",
+      authDomain: "auth-e3a9c.firebaseapp.com",
+      projectId: "auth-e3a9c",
+      storageBucket: "auth-e3a9c.appspot.com",
+      messagingSenderId: "264953494366",
+      appId: "1:264953494366:web:474366fc97f07dcb49adcf",
+      measurementId: "G-E6LGP34REC"
+    })
+  }
+}
+
+class App extends Component {
   render() {
     return (
       <View>
