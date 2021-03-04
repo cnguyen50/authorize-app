c4f6f8856c18cee5156dda54b2a7620aad775e60
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-03-03 20:01:01 -0800

    imported firebase and passed on press prop to login button

--- src/components/LoginFrom.js
+++ src/components/LoginFrom.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
+import firebase from 'firebase'
 import { Button, Card, CardSection, Input } from './common'
 
 class LoginForm extends Component {
   state = { email : '', password: '' };
+
+  onButtonPress() {
+    firebase.auth().signInWithEmailAndPassword()
+  }
 
   render() {
     return(
@@ -27,7 +32,7 @@ class LoginForm extends Component {
         />
         
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Log in
           </Button>
         </CardSection>
