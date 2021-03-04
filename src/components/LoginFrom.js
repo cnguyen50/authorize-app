bf717995c224998b986011a216a4ee8328dc0db7
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-03-03 20:09:47 -0800

    rendered error msg with Text from react native

--- src/components/LoginFrom.js
+++ src/components/LoginFrom.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import firebase from 'firebase'
 import { Button, Card, CardSection, Input } from './common'
+import { Text } from 'react-native'
 
 class LoginForm extends Component {
   state = { email : '', password: '', error: '' };
@@ -39,6 +40,10 @@ class LoginForm extends Component {
           
         />
         
+        <Text>
+          {this.state.error}
+        </Text>
+
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>
             Log in
